4ba203c7e6df7923422791d4cc660613fc7cc438
Karol Michonski <karolmichonsky@gmail.com>
2023-10-13 22:44:40 +0200

    Add BMI calculator functionality + fix styling

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navLinks.classList.toggle("active");
 })
+
+const heightCalculator = document.querySelector('input[name="height"]');
+const weightCalculator = document.querySelector('input[name="weight"]');
+const submitCalculator = document.querySelector('input[name="submit-calc"]');
+const bmiCalcResult = document.querySelector('.bmi-result');
+
+submitCalculator.addEventListener("click", () => {
+    const heightValue = heightCalculator.value;
+    const weightValue = weightCalculator.value;
+    const heightValueMeters = heightValue/100;
+    let bmiResult = weightValue/(heightValueMeters*heightValueMeters);
+    bmiResult = Math.round(bmiResult*100) /100;
+    bmiCalcResult.textContent = "Twój wskaźnik BMI wynosi: "+ bmiResult;
+})
