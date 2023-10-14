4fff678c4b7b1e83506581fa6ed7619c7fea5843
Karol Michonski <karolmichonsky@gmail.com>
2023-10-14 23:01:51 +0200

    Finish BMI calculator

--- script.js
+++ script.js
@@ -10,12 +10,36 @@ const heightCalculator = document.querySelector('input[name="height"]');
 const weightCalculator = document.querySelector('input[name="weight"]');
 const submitCalculator = document.querySelector('input[name="submit-calc"]');
 const bmiCalcResult = document.querySelector('.bmi-result');
+const bmiBodyType = document.querySelector('.bmi-body-type');
+
 
 submitCalculator.addEventListener("click", () => {
     const heightValue = heightCalculator.value;
     const weightValue = weightCalculator.value;
-    const heightValueMeters = heightValue/100;
-    let bmiResult = weightValue/(heightValueMeters*heightValueMeters);
-    bmiResult = Math.round(bmiResult*100) /100;
-    bmiCalcResult.textContent = "Twój wskaźnik BMI wynosi: "+ bmiResult;
+    const heightValueMeters = heightValue / 100;
+    let bmiResult = weightValue/(heightValueMeters * heightValueMeters);
+    bmiResult = Math.round(bmiResult * 100) /100;
+    if(validateForm(heightValue, weightValue)){
+        bmiCalcResult.textContent = "Twój wskaźnik BMI wynosi: " + bmiResult + " jest to " + findBodyType(bmiResult);
+    }
+    else{
+        bmiCalcResult.textContent = "Podano błędne dane.\n" + "Spróbuj ponownie. ";
+    }
 })
+
+function validateForm(heightValue, weightValue){
+    if(heightValue>=100 && heightValue<=260 && weightValue>=20){
+        return true;
+    }
+}
+
+function findBodyType(bmi){
+    const bmiRanges = [16, 17, 18.5, 25, 30, 35, 40];
+    const bmiResults = ["wyglodzenie", "wychudzenie", "niedowaga", "wartość prawidłowa", "nadwaga", "1 stopień otyłości", "2 stopień otyłości", "otyłość skrajna"];
+    for (let i = 0; i<bmiRanges.length; i++){
+        if(bmi<bmiRanges[i]){
+            return bmiResults[i];
+        } 
+    }
+    return bmiResults[bmiResults.length-1];
+}
